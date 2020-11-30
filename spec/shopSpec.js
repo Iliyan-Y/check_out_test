abe1443c702ade1fbd1a590fe589fed2410c0c39
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-30 13:57:04 +0000

    Tests for the rest of single letters values

--- spec/shopSpec.js
+++ spec/shopSpec.js
@@ -12,4 +12,12 @@ describe('Shop', () => {
   it('Shout return 30 for a single letter letter B', () => {
     expect(shop.checkout('B')).toEqual(30);
   });
+
+  it('Shout return 20 for a single letter letter C', () => {
+    expect(shop.checkout('C')).toEqual(20);
+  });
+
+  it('Shout return 15 for a single letter letter D', () => {
+    expect(shop.checkout('D')).toEqual(15);
+  });
 });
