431634f590db8902e5ae4860e3617effba12184b
Iliyan-Y <ili.yankov91@gmail.com>
2020-11-30 13:53:20 +0000

    Test for differen input value

--- spec/shopSpec.js
+++ spec/shopSpec.js
@@ -8,4 +8,8 @@ describe('Shop', () => {
   it('Shout return 50 for a single letter letter A', () => {
     expect(shop.checkout('A')).toEqual(50);
   });
+
+  it('Shout return 30 for a single letter letter B', () => {
+    expect(shop.checkout('B')).toEqual(30);
+  });
 });
